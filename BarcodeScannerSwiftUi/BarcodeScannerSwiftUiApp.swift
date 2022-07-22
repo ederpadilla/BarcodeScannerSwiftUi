122bcce111299d42174d636b49fe37088cb1ca03
€%er Padilla! <eder.padilla97@gmial.com>
2022-07-21 23:52:28 -0500

    Init project

--- BarcodeScannerSwiftUi/BarcodeScannerSwiftUiApp.swift
+++ BarcodeScannerSwiftUi/BarcodeScannerSwiftUiApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct BarcodeScannerSwiftUiApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BarcodeScannerView()
         }
     }
 }
