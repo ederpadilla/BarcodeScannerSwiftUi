05a50fc6711a7870d09d15b1c882b6cffe45c387
€%er Padilla! <eder.padilla97@gmial.com>
2022-07-21 23:49:40 -0500

    Initial Commit

---
+++ BarcodeScannerSwiftUi/BarcodeScannerSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerSwiftUiApp.swift
+//  BarcodeScannerSwiftUi
+//
+//  Created by Eder  Padilla on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
