3d27ef97d369ee93420f367fd2d21bb482ddc241
€%er Padilla! <eder.padilla97@gmial.com>
2022-07-22 00:00:23 -0500

    Init view structure

--- BarcodeScannerSwiftUi/BarcodeScannerView.swift
+++ BarcodeScannerSwiftUi/BarcodeScannerView.swift
@@ -9,8 +9,27 @@ import SwiftUI
 
 struct BarcodeScannerView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Rectangle()
+                    .foregroundColor(.gray)
+                    .frame(maxWidth: .infinity,
+                           maxHeight: 300)
+                
+                Spacer().frame(height: 60)
+                
+                Label("Scanned Barcode",
+                      systemImage: "barcode.viewfinder")
+                .font(.title)
+                .padding()
+                
+                Text("Not scanned yet")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(.red)
+            }
+            .navigationTitle("👓Barcode Scanner")
+        }
     }
 }
 
